fce2b1805e592907aba0fc1a9b2daf6815d0ae9a
Antti Keskinen <32198867+anttikes@users.noreply.github.com>
2025-12-20 12:10:10 +0200

    GUI file formatting

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -4,14 +4,15 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-
     QQmlApplicationEngine engine;
+
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
+
     engine.loadFromModule("GUI", "Main");
 
     return app.exec();
