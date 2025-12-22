bfa84070926fe51b0c0c95a4a869b9b23175147d
Antti Keskinen <32198867+anttikes@users.noreply.github.com>
2025-12-23 00:22:06 +0200

    Reorganized GUI files a bit

--- GUI/src/main.cpp
+++ GUI/src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine(&app);
 
-    app.setWindowIcon(QIcon(":/images/icon.png"));
+    app.setWindowIcon(QIcon(":/resources/icon.png"));
 
     engine.loadFromModule("GUI", "Main");
 
