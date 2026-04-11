7955f66cade6d0ad3dbba0513a23ca7014b420f9
Antti Keskinen <32198867+anttikes@users.noreply.github.com>
2026-04-11 09:09:21 +0300

    Switched I2C to fast mode

--- Firmware/Core/i2c.c
+++ Firmware/Core/i2c.c
@@ -23,7 +23,7 @@ I2C_HandleTypeDef hi2c1;
 void MX_I2C1_Init(void)
 {
     hi2c1.Instance = I2C1;
-    hi2c1.Init.Timing = 0x00D095D9;
+    hi2c1.Init.Timing = 0x0070133B;
     hi2c1.Init.OwnAddress1 = 0;
     hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
     hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
